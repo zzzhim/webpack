d1dcb3de0d7c2e326e4cbfb5ec8b3f79f090f1ae
wangyafei <3218566389@qq.com>
2019-03-13 21:32:37 +0800

    webpack使用插件

---
+++ lesson-demo4/src/index.js
@@ -0,0 +1,5 @@
+import './index.scss'
+
+const root = document.getElementById('app')
+
+root.innerHTML = '<div class="iconfont icon-changjingguanli"></div>'
---
+++ lesson-demo4/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+
+// HtmlWebpackPlugin 会在打包结束后，自动生成一个html文件，并把打包生成的js自动引入到这个html文件中
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    // 打包后是否压缩
+    mode: "development", // "production" | "development" | "none"
+    // 应用程序执行的文件
+    entry: "./src/index.js",
+    output: {
+        // 输出的文件名
+        filename: "dist.js",
+        // 输出文件的目录
+        path: path.resolve(__dirname, 'bundle')
+    },
+    // 关于模块配置
+    module: {
+        // 模块规则（配置 loader、解析器等选项）
+        rules: [
+            {
+                test: /\.(eot|ttf|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                }
+            },
+            {
+                // loader的执行顺序是从下到上，从右到左的执行顺序
+                test: /\.(scss)$/ig,
+                // 打包sass文件
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2
+                        }
+                    },
+                    'sass-loader',
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+    // 使用插件
+    // plugin 可以在webpack运行到某个时刻的时候，帮你做一些事情
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 模板
+            template: 'src/index.html'
+        }),
+        // 清空整个dist目录
+        new CleanWebpackPlugin({
+            options: 'dist'
+        })
+    ]
+}
